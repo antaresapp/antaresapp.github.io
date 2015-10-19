9f1f933cff4b9e0227779c15435c4ad8f4cfc493
Wern Ancheta <antaresapp@gmail.com>
2015-10-19 12:22:56 +0800

    1445228576

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,17 +1,101 @@
-(function(){
+var items_template = Handlebars.compile($('#items-template').html());
+var categories_template = Handlebars.compile($('#categories-template').html());
+var categories_mobile_template = Handlebars.compile($('#categories-mobile-template').html());
 
-	var path = window.location.pathname;
-	if(path == '/'){
-		path = '/hn';
+var path = window.location.pathname;
+if(path == '/'){
+	path = '/hn';
+}
+$('a[href="' + path + '"]').addClass('active');
+
+$('#items').on('click', '.item-link', function(){
+	$(this).siblings('.actions').children('a').removeClass('hidden');
+});
+
+$('#news_sources').change(function(){
+	var value = $(this).val();
+	window.location.href = value;
+});
+
+var categories = store.get('categories');
+
+var category = 'hn';
+var path_array = window.location.pathname.split('/');
+if(path_array[1] != ''){
+	category = path_array[1];
+}
+
+$("a[href='/" + category + "']").addClass('active');
+$("option[value='/" + category + "']").prop('selected', true);
+
+var current_category = _.find(categories, function(cat) {
+  return cat.route == category;
+});
+
+var enabled_sources = _.filter(current_category.sources, function(src){
+  return src.enabled;
+});
+
+var enabled_categories = _.filter(categories, function(cat){
+	return cat.enabled;
+});
+
+var categories_html = categories_template({'categories': enabled_categories});
+$('#nav ul').html(categories_html);
+
+var categories_mobile_html = categories_mobile_template({'categories': enabled_categories});
+$('#mobile-nav ul').html(categories_mobile_html);
+
+var requests = [];
+
+var base_url = 'http://antaresapp.space/json/';
+
+
+if(enabled_sources.length > 0){
+	for(var x in current_category.sources){
+
+		var source = current_category.sources[x];
+		if(source.enabled){		
+
+			requests.push(
+				$.getJSON(base_url + source.route + '.json')
+			);
+		}
 	}
-	$('a[href="' + path + '"]').addClass('active');
+}else{	
+	requests.push(
+		$.getJSON(base_url + current_category.route + '.json')
+	);
+}
 
-	$('#items').on('click', '.item-link', function(){
-		$(this).siblings('.actions').children('a').removeClass('hidden');
-	});
 
-	$('#news_sources').change(function(){
-		var value = $(this).val();
-		window.location.href = value;
-	});
-})();
+$.when.apply($, requests).then(function(){
+	args = arguments;
+	var news_items = [];
+	
+	if(enabled_sources.length > 1){
+		for(var x in arguments){
+			if(arguments[x] instanceof Array){		
+				var items = arguments[x][0];
+				
+				for(var y in items){
+					var item = items[y];
+					news_items.push(item);
+				}
+			}
+		}
+	}else if(enabled_sources.length <= 1){
+		
+		var items = arguments[0];
+		for(var y in items){
+			var item = items[y];
+			news_items.push(item);
+		}
+	}
+
+	var items_html = items_template({'items': news_items});
+	$('#items').html(items_html).removeClass('hidden');
+
+});
+
+	
---
+++ assets/js/settings.js
@@ -0,0 +1,84 @@
+setSettings();
+
+function setSettings(){
+	var categories = store.get('categories');
+	for(var x in categories){
+	  var cat = categories[x];
+	  var route = cat['route'];
+	  var enabled = cat['enabled'];
+	  
+	  var sources = cat['sources'];
+	  if(sources.length){
+	    for(var y in sources){
+	      var source_route = sources[y]['route'];
+	      var source_enabled = sources[y]['enabled'];
+	      if(source_enabled){
+	        $("input[name=" + source_route + "]").prop('checked', true);
+	      }else{
+	        $("input[name=" + source_route + "]").prop('checked', false);
+	      }
+	    }
+	  }
+	  if(enabled){
+	    $("input[name=" + route + "]").prop('checked', true);
+	  }else{
+	    $("input[name=" + route + "]").prop('checked', false);
+	  }
+	 
+	}
+}
+
+function setCategories(){
+
+	var categories = [];
+	$('.category').each(function(){
+		var category = $(this);
+		var name = category.data('name');
+		var route = category.attr('name');
+		
+		var enabled = false;
+		if(category.is(':checked')){
+			enabled = true;
+		}
+
+		var sources = [];
+		category.parents('li:eq(0)').find('.sources input').each(function(){
+			var self = $(this);
+			var source_name = self.data('name');
+			var source_route = self.attr('name');
+			var source_enabled = false;
+			if(self.is(':checked')){
+				source_enabled = true;
+			}
+
+			sources.push({
+				'name': source_name,
+				'route': source_route,
+				'enabled': source_enabled
+			});
+			
+		});
+
+		categories.push({
+			'name': name,
+			'route': route,
+			'enabled': enabled,
+			'sources': sources
+		});
+		
+	});
+
+	store.set('categories', categories);
+}
+
+$('.category').click(function(){
+
+	setCategories();
+
+});
+
+$('.source').click(function(){
+
+	setCategories();
+
+});
